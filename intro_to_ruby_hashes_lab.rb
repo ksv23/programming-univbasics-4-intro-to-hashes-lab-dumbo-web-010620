2ed1808fec173b03a936ea18e42f71239552bd6b
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-23 23:13:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,6 +29,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+  :name => 'Grace Hopper'
 end
 
 def read_from_hash(hash, key)
