a3a86995997c55bfb9254cd9211ba7f76aadf600
Kring Villanueva <ksvillanueva7@gmail.com>
2019-12-23 23:14:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,9 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  :name => 'Grace Hopper'
+  my_hash_creator = {
+    :name => 'Grace Hopper'
+  }
 end
 
 def read_from_hash(hash, key)
